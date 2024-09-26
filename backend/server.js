46196a8baf2a64da7d1b69771540e7496e316483
ashraf-786ash <shaikashraf1278612@gmail.com>
2024-09-26 21:23:17 +0530

    online status

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ import userRoutes from "./routes/userRoutes.js";
 
 
 import connectToMongoDB from './db/connectTOMongoDB.js';
+import { app, server } from './socket/socket.js';
 
 
-const app=express();
 const PORT= process.env.PORT || 5000;
 
 dotenv.config();
@@ -30,7 +30,7 @@ app.use("/api/users",userRoutes);
   //  res.send("Hello world!")
 //});
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`)
 });
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom'; // Correct import for BrowserRouter
 import { AuthContextProvider } from './context/Authcontext';
+import { SocketContextProvider } from './context/Socketcontex';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <AuthContextProvider>
+      <SocketContextProvider>
       <App />
+      </SocketContextProvider>
     </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>
