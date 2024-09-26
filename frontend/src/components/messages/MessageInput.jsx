c0990191c1d9e89f4c50ede6d274d4e4233d155d
ashraf-786ash <shaikashraf1278612@gmail.com>
2024-09-26 10:57:20 +0530

    send message

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSend } from 'react-icons/bs';
+import useSendMessage from '../../hooks/useSendMessage';
 
 const MessageInput = () => {
+
+const [message,setMessage]=useState("")
+const {loading,sendMessage}=useSendMessage()
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    if(!message) return;
+    await sendMessage(message);
+    setMessage("")
+
+  };
   return (
-    <form className='px-4 my-3'>
+    <form className='px-4 my-3'onSubmit={handleSubmit}>
       <div className='flex items-center border text-sm rounded-lg bg-gray-700 border-gray-600 text-white'>
         {/* Text Input */}
         <input
           type="text"
           className='flex-1 p-2.5 bg-gray-700 text-white outline-none'
           placeholder='Send a message'
+          value={message}
+          onChange={(e)=>setMessage(e.target.value)}
         />
         {/* Send Button */}
         <button type='submit' className='px-3 text-white'>
-          <BsSend size={20} />
+          {loading?<div className='loading loading-spinner'></div>:<BsSend size={20} />}
+          
         </button>
       </div>
     </form>
--- frontend/src/zustand/useConversation.jsx
+++ frontend/src/zustand/useConversation.jsx
@@ -2,6 +2,7 @@ import {create} from "zustand";
 
 const useConversation=create((set)=>({
     selectedConversation:null,
+    messages:[],
     setSelectedConversation:(selectedConversation)=> set({selectedConversation}),
     setMessages:(messages)=>set({messages}),
 
